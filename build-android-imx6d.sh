03e9c0d8cd92ff8066ddf0471083eef18f5c389d
Kyosuke Nekomura <supercatexpert@gmail.com>
2018-12-17 12:39:35 +0900

    Added missing Android build script.

---
+++ build-android-imx6d.sh
@@ -0,0 +1,6 @@
+export ARCH="arm"
+export CORES=`getconf _NPROCESSORS_ONLN`
+export CROSS_COMPILE="arm-linux-androideabi-"
+export PATH=/opt/arm-linux-androideabi-4.9/bin:$PATH
+make mx6dsabresdandroid_luna_tv_defconfig
+make -j2
