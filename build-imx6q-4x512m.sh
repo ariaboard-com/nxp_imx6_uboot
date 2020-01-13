f0a1faae8461745d307a31dd0ab33485ec1546fc
Kyosuke Nekomura <supercatexpert@gmail.com>
2020-01-13 13:59:35 +0900

    Added missing build script.

---
+++ build-imx6q-4x512m.sh
@@ -0,0 +1,20 @@
+export ARCH="arm"
+export CORES=`getconf _NPROCESSORS_ONLN`
+export CROSS_COMPILE="arm-linux-gnueabihf-"
+
+#export PATH="gcc-linaro-5.3.1-2016.05-x86_64_arm-linux-gnueabihf/bin:/opt/gcc-linaro-arm-linux-gnueabihf-4.8-2013.06_linux/bin:/opt/gcc-linaro-arm-linux-gnueabihf-4.8-2013.06_linux/bin:$PATH"
+
+if [ x"$1" = x"mrprober" ]; then
+   rm -rf build/imx6q-4x512m
+fi
+
+if [ ! -d build/imx6q-4x512m ]; then
+    mkdir -p build/imx6q-4x512m
+fi
+
+make O=build/imx6q-4x512m mx6qsabresd_luna_monitor_defconfig
+make O=build/imx6q-4x512m -j2
+
+rm -rf deploy/imx6q-4x512m
+mkdir -p deploy/imx6q-4x512m
+cp build/imx6q-4x512m/u-boot-dtb.imx deploy/imx6q-4x512m/
